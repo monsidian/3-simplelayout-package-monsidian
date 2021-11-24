31e1a3b6c27de1971f9a92a876e90e2266b8f6ff
monsidian <403216817@qq.com>
2021-11-24 10:56:22 +0800

    10:56

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,13 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src'))
 
 project = '3-simplelayout-package-monsidian'
 copyright = '2021, Yutong Long'
