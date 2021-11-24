2cba425c0ee2dbbdf6474ab933fd1d34f21fdd52
monsidian <403216817@qq.com>
2021-11-24 17:04:57 +0800

    17:04

--- bak/docs/source/conf.py
+++ bak/docs/source/conf.py
@@ -34,6 +34,12 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
